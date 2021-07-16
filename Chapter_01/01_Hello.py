020520d51892d89921269b69dc7fba76ddf6f184
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-16 09:42:59 +0500

    Add files via upload

---
+++ Chapter_01/01_Hello.py
@@ -0,0 +1,12 @@
+#this "#" is a single line comment and ''' this is multi-line comment  
+#This is my Hellow world Fisrt program in python
+''' 
+********************************
+Author: Deepak Lal
+Eletronics Engieer
+Sukkur IBA University
+********************************
+ '''
+
+import os   #importing the OS module
+print ("hello world ")
