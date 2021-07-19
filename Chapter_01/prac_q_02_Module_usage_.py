2ee35fa49b3ff3b7152f920c9cdc86f7f1f94c67
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-19 16:39:59 +0500

    Add files via upload

---
+++ Chapter_01/prac_q_02_Module_usage_.py
@@ -0,0 +1,5 @@
+#Author: Deepak Lal
+#Sukkur IBA University
+#this is a simple program for using and downloading of modules using PIP in terminal
+from playsound import playsound
+playsound('C:\\Users\\deepa\\Desktop\\python\\Chapter_01\\Nindiya_re.mp3stop')
