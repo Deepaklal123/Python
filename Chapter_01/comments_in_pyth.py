5f6260359e12bf91d60b376df3b85d4ddf15d473
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-19 16:27:29 +0500

    Add files via upload

---
+++ Chapter_01/comments_in_pyth.py
@@ -0,0 +1,13 @@
+#Deepak Lal
+#Sukkur IBA University
+
+# this is a single line comment in python programming
+#if you want to know how to make multiple line comments 
+#so its given below
+'''
+Hi this is multi-line comment section,
+we used three single qoutes for start 
+and other for the ending or termination 
+of multi-line commment
+
+'''
