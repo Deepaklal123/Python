d1068546110c2490a8a489f31d0b294005efbd9d
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-19 16:34:34 +0500

    Add files via upload

---
+++ Chapter_01/prac_q_01.py
@@ -0,0 +1,14 @@
+'''
+Author:     Deepak Lal
+Sukkur IBA University     
+'''
+
+#This is a simple program for writing a twinkle twinkle poem
+#Since, this is a multi-line print program of poem
+#therefor, likewise multi-line comment, we have to use ''' ''' instead of " "
+print(''' Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle little star
+How I wonder what you are''')
