971ad31d415f089e5a48de9da8706ba99544c656
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-20 12:29:07 +0500

    Add files via upload

---
+++ Chapter_02/prac_q_01_variables_.py
@@ -0,0 +1,19 @@
+#Author: Deepak Lal
+#sukkur IBA University
+a='Hello'             #this is a string variable stores Hello
+#a="Hello"
+#a='''Hello'''
+x=  ''' "Hello"
+    i am 
+    a python
+    simple program
+    for using triple single qoutes
+    in order to print 
+    new lines strings  '  '''  #also a variable but with double qoutes, hence used triple signle qoutes   
+
+b=345
+c=45.56
+print(a)
+print(x)
+print (b)
+print(c)
