a57b5421ccafb69f676822f77f28bc90bea5d08b
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-25 11:09:27 +0500

    Add files via upload

---
+++ Chapter_02/prac_q_03_type_casting.py
@@ -0,0 +1,14 @@
+#Name of Author: Deepak Lal
+# Learning Python 
+
+
+a="546"     #This is string type
+b=546       #This is int type
+c=False     #This is boolean
+d=2.545     #This is float
+#let's try to find out how to know the type of variable
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
