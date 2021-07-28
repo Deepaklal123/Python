2c60d59ecfcb9b7446e2e8aba1d54d1d6f8d5c71
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-28 18:45:55 +0500

    Add files via upload

---
+++ Chapter_02/prac_q_06_average.py
@@ -0,0 +1,9 @@
+#Author: Deepak Lal
+#Sukkur IBA University
+#program for finding average of two numbers
+a=input("Enter a number :   ")
+b=input("Enter another number :    ")
+a=int(a)
+b=int(b)
+avg=(a+b)/2
+print("The average of a and b is : ",avg )
