d9d06337a5bc33c6038e77266e8b333a7c4da164
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-28 18:36:42 +0500

    Add files via upload

---
+++ Chapter_02/prac_q_04_input_function.py
@@ -0,0 +1,8 @@
+#Author:  Deepak Lal
+# Sukkur IBA University
+a= input(" Enter your name ")   #This alwaays takes inpt as string
+print(a)
+
+num1= input(" Enter your age ")   #This alwaays takes inpt as string
+num1=int(num1)
+print(num1)
