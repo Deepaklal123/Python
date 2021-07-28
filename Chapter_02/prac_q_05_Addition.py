97350047df3f1e00d868600087bf18e6f98a84d4
Deepak Lal <79632332+Deepaklal123@users.noreply.github.com>
2021-07-28 18:37:20 +0500

    Add files via upload

---
+++ Chapter_02/prac_q_05_Addition.py
@@ -0,0 +1,13 @@
+#Author : Deepak Lal
+#Sukur IBA University |Pakistan 
+a=34
+b=12
+print("The sum of a and b is :   ", a+b)
+
+#or i can take input of values of a & b from user  
+a=input("Enter the value of a  :  ")
+b=input("Enter the value of b  :  ")
+a=int(a)                            #converting to integer from str
+b=int(b)                            #converting to integer from str                         
+sum=a+b
+print("Sum of a & b is       : ", sum) 
